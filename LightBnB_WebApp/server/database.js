03578eafe41502e7a3d78acbe7598328d36a8272
Roylu7384 <roylu7384@gmail.com>
2022-03-10 12:31:57 -0500

    Refactor getAllReservations function to connect to postgres database

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -86,7 +86,29 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+  const values = [guest_id, limit];
+  
+  const queryStr = `
+  SELECT reservations.*, properties.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `;
+
+  return pool.query(queryStr, values)
+    .then(res => {
+      console.log(res.rows);
+      return res.rows;
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+
 };
 exports.getAllReservations = getAllReservations;
 
